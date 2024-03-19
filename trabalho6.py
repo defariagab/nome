a6d7b8f78e3b364de75ad325253b0fcb3b546dda
defariagab <160599651+defariagab@users.noreply.github.com>
2024-03-19 10:32:08 -0300

    Add files via upload

---
+++ trabalho6.py
@@ -0,0 +1,41 @@
+# Variáveis para armazenar 4 números
+numero1 = 3
+numero2 = 1
+numero3 = 4
+numero4 = 1
+
+# Variável para armazenar uma frase
+frase = "Minha frase"
+
+# Variável para armazenar uma palavra
+palavra = "trabalho"
+
+# Média aritmética dos quatro números
+media = (numero1 + numero2 + numero3 + numero4) / 4
+
+# Quadrado de um dos números (número1)
+quadrado = numero4 ** 2
+
+# Dobro de um dos números (número2)
+dobro = numero3 * 2
+
+# Quantidade de letras na palavra
+quantidade_letras_palavra = len(palavra)
+
+# Quantidade de espaços em branco na frase
+quantidade_espacos_frase = frase.count(' ')
+
+# Verifica se o primeiro número é maior que o segundo
+primeiro_maior_que_segundo = numero2 > numero4
+
+# Encontra o maior número entre os quatro
+maior_numero = max(numero1, numero2, numero3, numero4)
+
+# Imprime os resultados
+print(media)
+print(quadrado)
+print(dobro)
+print(quantidade_letras_palavra)
+print(quantidade_espacos_frase)
+print(primeiro_maior_que_segundo)
+print(maior_numero)
