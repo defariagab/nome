f7ac2c0b65bbafeef7c3cb5e1a80f7996e72edb7
defariagab <160599651+defariagab@users.noreply.github.com>
2024-05-21 09:39:29 -0300

    Add files via upload

---
+++ trabalho_4.py
@@ -0,0 +1,18 @@
+
+# Exemplo de código Python com estrutura de repetição e tomada de decisão
+
+# Estrutura de repetição: Loop for
+# Este código imprime os números de 1 a 10
+for i in range(1, 11):
+    print(i)
+
+# Estrutura de tomada de decisão: if-elif-else
+# Este código verifica se um número é positivo, negativo ou zero
+numero = 5
+
+if numero > 0:
+    print("O número é positivo.")
+elif numero < 0:
+    print("O número é negativo.")
+else:
+    print("O número é zero.")
